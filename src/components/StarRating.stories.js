5f927438dcc528752a566b5952788d8e7a888904
Ivan Ryazanov <i.riazanov@napoleonit.ru>
2022-10-10 19:09:02 +0600

    added storybook

---
+++ src/components/StarRating.stories.js
@@ -0,0 +1,42 @@
+import StarRating from './StarRating.vue';
+
+export default {
+  title: 'StarRating',
+  component: StarRating,
+  argTypes: {
+    nameRating: String,
+    totalStars: Number,
+    sizeStars: Number,
+    activeColor: String,
+    inactiveColor: String,
+    readonly: Boolean
+  },
+};
+
+const Template = (args) => ({
+  components: { StarRating },
+  setup() {
+    return { args };
+  },
+  template: '<StarRating v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+};
+
+export const Readonly = Template.bind({});
+Readonly.args = {
+  readonly: false,
+};
+
+export const Colors = Template.bind({});
+Colors.args = {
+  activeColor: '#3f3f3f',
+  inactiveColor: '#c3c3c3',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  sizeStars: 20,
+};
